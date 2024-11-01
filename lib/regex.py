61f4c0e59a9a23a426cfcf5d79e33f4463007ae9
William Gitau <dontysh@gmail.com>
2024-11-02 00:27:50 +0300

    passes all tests

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Regex for names: allows letters, spaces (only single space allowed), hyphens, and apostrophes, must start with an uppercase letter
+name = r"^(?!.*  )[A-Z][a-zA-Z' -]+$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Regex for phone numbers: supports formats like 5555555555, 555-555-5555, (555) 555-5555
+phone_number = r"^(?:\(\d{3}\)\s?|\d{3}[-]?)(\d{3})[-]?(\d{4})$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Regex for email addresses: follows common patterns for valid emails
+email_address = r"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 email_regex = re.compile(email_address)
